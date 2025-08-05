f36ba05623dce3230b69023774a648ad641ca804
Viktor Kotuliak <viktor.kotuliak@gmail.com>
2025-08-05 14:52:16 +0200

    Fixed small error in analyze_citation file. Updated README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 To work with the **ClinVar** database you first need to download it through following [link](https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/). After downloading, you can use file `clinvar_xml_to_csv.py` to turn it into csv (you will need to change the name of file to *clinvar_dataset.xml.gz* or change it in code).
 
+To extract all the abstracts from PubMed database, which will also be used for training of the model, use file `fetch_pubmed_abstracts.py`. This will take all the unique pubmed references from the file `clinvar_csv_dataset.csv` and load abstract for each paper. We have decided to work only with PubMed articles, since they comprise majority of all the citations. To check this yourself, you can use file `analyze_citation_sources.py`.
+
 You can use file `make_graphics.py` to draw different graphics about the csv with clinvar data.
 
 There are also files `clinvar_preview.py` and `example.xml` which are just short snippets from the original xml saved in clinvar database.
--- analyze_citation_sources.py
+++ analyze_citation_sources.py
@@ -39,14 +39,57 @@ def analyze_sources(in_file):
         print(f"PubMed citations: {pubmed}")
         print(f"Other citations: {other}")
 
-
-if __name__ == "__main__":
-    in_file = "clinvar_dataset.xml.gz"
-    analyze_sources(in_file)
-
 """
 Results:
 PubMed citations: 1019311
 Other citations: 2728
 Ratio of PubMed to other sources: 373:1
 """
+
+def count_all_occurences(in_file):
+    """Count all occurrences of citations in the XML file."""
+    count = 0
+    with gzip.open(in_file, "rb") as f:
+        context = etree.iterparse(f, events=("end",))
+
+        pubmed_arcs = set()
+        other_arcs = set()
+
+        for _, elem in context:
+            if elem.tag == "VariationArchive":
+                citation_elems = elem.findall(".//Citation")
+                for cit in citation_elems:
+                    id_elems = cit.findall("./ID")
+                    for id_elem in id_elems:
+                        source = id_elem.get("Source")
+                        if source and source.lower() == "pubmed":
+                            pubmed_arcs.add(id_elem.text)
+                        else:
+                            other_arcs.add(id_elem.text)
+
+                count += 1
+                if count % 1000 == 0:
+                    print(f"Processed {count} elements")
+
+                elem.clear()
+                while elem.getprevious() is not None:
+                    del elem.getparent()[0]
+        del context
+        print(f"Done. Total {count} elements processed")
+        print(f"Total unique PubMed citations: {len(pubmed_arcs)}")
+        print(f"Total unique Other citations: {len(other_arcs)}")
+    return count
+
+"""
+Results:
+Total 3595245 elements processed
+Total unique PubMed citations: 128471
+Total unique Other citations: 1864
+Ratio of PubMed to other sources: 69:1
+"""
+
+if __name__ == "__main__":
+    in_file = "clinvar_dataset.xml.gz"
+    # in_file = "example.xml"
+    # analyze_sources(in_file)
+    count_all_occurences(in_file)
